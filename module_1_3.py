2525c9aa473a2487dc2b73cb37f822cad8453641
Николай <dhyeheydddjeujd>
2024-09-30 19:01:19 +0300

    30/09

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Nikolai"
+print(name, type(name))
+age = 21
+print(age, type(age))
+age = 21
+new_age =age + 1
+print( new_age, type(new_age))
+is_student = 10 > 5
+print(is_student, type(is_student))
